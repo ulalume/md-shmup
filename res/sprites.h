33d93762299563fce482c03b4126b4f4651b39bb
kbt <kubota@softdevice.co.jp>
2021-04-05 19:26:17 +0900

    add bullets

--- res/sprites.h
+++ res/sprites.h
@@ -2,5 +2,6 @@
 #define _RES_SPRITES_H_
 
 extern const SpriteDefinition player_sprite;
+extern const SpriteDefinition bullet_sprite;
 
 #endif // _RES_SPRITES_H_
--- res/sprites.res
+++ res/sprites.res
@@ -1,1 +1,2 @@
 SPRITE player_sprite "sprites/player.png" 2 2 0 3
+SPRITE bullet_sprite "sprites/bullet.png" 1 1 0 3
