b8e815a24b1a754b269646ba2a58c6d143e791f1
kbt <kubota@softdevice.co.jp>
2021-04-05 22:07:16 +0900

    BGを大きくすることで変な動きを回避

--- src/stage1_background.c
+++ src/stage1_background.c
@@ -2,6 +2,8 @@
 #include <genesis.h>
 #include "stage1_background.h"
 
+#include "gfx.h"
+
 #define BG_CELL_WIDTH 64
 #define BG_CELL_HEIGHT 32
 #define BG_WIDTH (BG_CELL_WIDTH * 8)
@@ -9,19 +11,15 @@
 
 Stage1Background *Stage1Background_create(int paletteIndex, const Palette *palette, VDPPlane bg, const MapDefinition *mapDef, int x, int y, int velx, int vely)
 {
-  SYS_disableInts();
   VDP_setPaletteColor(63, RGB24_TO_VDPCOLOR(0x9292b6));
   VDP_setBackgroundColor(63);
 
   VDP_setPalette(paletteIndex, palette->data);
+
   VDP_setPlaneSize(BG_CELL_WIDTH, BG_CELL_HEIGHT, FALSE);
   VDP_loadTileSet(mapDef->tileset, TILE_USERINDEX, DMA);
-
+  //VDP_loadTileSet(mapDef->tileset, TILE_USERINDEX, DMA);
   Map *map = MAP_create(mapDef, bg, TILE_ATTR_FULL(paletteIndex, FALSE, FALSE, FALSE, TILE_USERINDEX));
-  //u16 tilemapBuf[64 * 32 * 2];
-
-  //MAP_getTilemapRect(map, 0, 0, 64, 16, TRUE, tilemapBuf);
-  //VDP_setTileMapData(bg, tilemapBuf, 0, 32 * 16, 32, DMA);
 
   Stage1Background *b = (Stage1Background *)MEM_alloc(sizeof(Stage1Background));
   b->bg = bg;
@@ -30,14 +28,13 @@ Stage1Background *Stage1Background_create(int paletteIndex, const Palette *palet
   b->velx = velx;
   b->vely = vely;
   b->map = map;
-  SYS_enableInts();
 
   return b;
 }
 void Stage1Background_update(Stage1Background *b)
 {
-  b->x = (b->x + b->velx); //+ BG_WIDTH) % BG_WIDTH;
-  b->y = (b->y + b->vely); //+ BG_HEIGHT) % BG_HEIGHT;
+  b->x = (b->x + b->velx + BG_WIDTH * 2) % BG_WIDTH - BG_WIDTH;
+  b->y = (b->y + b->vely + BG_HEIGHT * 2) % BG_HEIGHT - BG_HEIGHT;
 
   MAP_scrollTo(b->map, -b->x, -b->y);
 }
